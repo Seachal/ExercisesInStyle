d4cc789a9b76dd7d67013dc87abbf2da145ea245
Marco Faella <marfaella@gmail.com>
2019-07-22 19:48:15 +0200

    Appendix files added

---
+++ src/eis/appendix/nowhitespace/Container.java
@@ -0,0 +1,1 @@
+public class Container{float a,s,t;Container n=this,c=n;public float getAmount(){for(s=t=0;s<1||c!=n;c=c.n,s++)t+=c.a;return t/s;}public void connectTo(Container o){c=o.n;o.n=o.c=n;n=c;}public void addWater(float w){a+=w;}}
